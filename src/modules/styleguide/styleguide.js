ab15690c41a4d3bd0308df93540e809a2c31b6cd
Jon Hendershot <jon@ruckusco.com>
2021-03-10 10:21:18 -0500

    Add initial style guide module

---
+++ src/modules/styleguide/styleguide.js
@@ -0,0 +1,10 @@
+/**
+ * Initializes the site's styleguide module.
+ * @constructor
+ * @param {Object} el - The site's styleguide container element.
+ */
+const styleguide = el => {
+  // console.log(el)
+}
+
+export default styleguide
