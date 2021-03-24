98cd3ff2d312ff2c1ebc77aea36257fa1d1fea0d
Xuan Thai Do <thai.do@solislab.com>
2021-03-24 16:42:09 +0700

    DIC-5 Add footer cta markup and style

---
+++ src/modules/form-subscribe/form-subscribe.js
@@ -0,0 +1,10 @@
+/**
+ * Initializes the site's formsubscribe module.
+ * @constructor
+ * @param {Object} el - The site's formsubscribe container element.
+ */
+const formsubscribe = el => {
+  // console.log(el)
+}
+
+export default formsubscribe
