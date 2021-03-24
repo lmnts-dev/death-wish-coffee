b2dad2511c86c7a797be3f6760817fd25b497270
Xuan Thai Do <thai.do@solislab.com>
2021-03-24 17:32:02 +0700

    DIC-5 Add validation for subscribe form

--- src/modules/form-subscribe/form-subscribe.js
+++ src/modules/form-subscribe/form-subscribe.js
@@ -3,8 +3,25 @@
  * @constructor
  * @param {Object} el - The site's formsubscribe container element.
  */
+
+import { validateInput } from 'lib/util'
+
 const formsubscribe = el => {
-  // console.log(el)
+  const submitEl = el.querySelector('.js-submit')
+  const inputEls = el.querySelectorAll('.js-input-field input')
+  if (submitEl) {
+    submitEl.addEventListener('click', (e) => {
+      let isValid = true
+      if (inputEls.length) {
+        for (let i = 0; i < inputEls.length; i++) {
+          isValid = isValid && validateInput(inputEls[i])
+        }
+      }
+      if (!isValid) {
+        e.preventDefault()
+      }
+    })
+  }
 }
 
 export default formsubscribe
