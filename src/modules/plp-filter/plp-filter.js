5cda3e180739c1a2d3902864a5034120482558a3
Xuan Thai Do <thai.do@solislab.com>
2021-04-06 16:59:25 +0700

    DIC-34 Add functionality for plp filter

--- src/modules/plp-filter/plp-filter.js
+++ src/modules/plp-filter/plp-filter.js
@@ -3,8 +3,21 @@
  * @param {Object} el - The DOM Node containing the data-module="plp-filter" attribute.
  */
 
+import { toggle } from 'lib/util'
+
 const plpFilter = (el) => {
-  console.warn('initializing plpFilter module')
+  const filterBlocks = el.querySelectorAll('.js-filter-block')
+  const filterBlockActiveClass = 'active'
+  if (filterBlocks.length) {
+    filterBlocks.forEach(block => {
+      const title = block.querySelector('.js-filter-block-title')
+      if (title) {
+        title.addEventListener('click', () => {
+          toggle(block, filterBlockActiveClass)
+        })
+      }
+    })
+  }
 }
 
 export default plpFilter
---
+++ src/modules/plp/plp.js
@@ -0,0 +1,10 @@
+/**
+ * Initializes the site's plp module.
+ * @constructor
+ * @param {Object} el - The site's plp container element.
+ */
+const plp = el => {
+  // console.log(el)
+}
+
+export default plp
