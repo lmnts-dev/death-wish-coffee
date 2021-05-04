351adbd67bbcf89aede0b2d1413316a313d5c266
Xuan Thai Do <thai.do@solislab.com>
2021-05-04 14:12:38 +0700

    DIC-15 Integrate product data for product card

--- src/modules/product-card/product-card.js
+++ src/modules/product-card/product-card.js
@@ -9,6 +9,10 @@ const productCard = el => {
   const buttonEl = el.querySelector('.js-button')
   const closeButton = el.querySelector('.js-close')
   const overlay = el.querySelector('.js-overlay')
+  const inputEls = el.querySelectorAll('.js-product-input')
+  const variantEl = el.querySelector('.js-product-variants')
+  const submitButtonEl = el.querySelector('.js-form-submit')
+  const options = {}
 
   if (buttonEl) {
     buttonEl.addEventListener('click', () => {
@@ -20,6 +24,31 @@ const productCard = el => {
     closeButton.addEventListener('click', () => {
       toggle(overlay, 'is-active')
     })
+  }
+
+  if (inputEls.length && variantEl) {
+    for (const inputEl of inputEls) {
+      inputEl.addEventListener('change', () => {
+        const optionIndex = inputEl.getAttribute('data-option-index')
+        const optionKey = `option${optionIndex}`
+        options[optionKey] = inputEl.value
+        const selectedTitle = Object.keys(options).sort().map(key => options[key]).join(' / ')
+        let selectedValue = null
+        for (const optionEl of variantEl.options) {
+          if (optionEl.getAttribute('data-title') === selectedTitle) {
+            selectedValue = optionEl.value
+            optionEl.selected = true
+          }
+        }
+        if (selectedValue) {
+          variantEl.value = selectedValue
+
+          if (submitButtonEl) {
+            submitButtonEl.removeAttribute('disabled')
+          }
+        }
+      })
+    }
   }
 }
 
