09529f820136db074ecfd2e5ea006a2dd1f967bb
Xuan Thai Do <thai.do@solislab.com>
2021-05-07 10:48:39 +0700

    DIC-17 Update price display on pdp

--- src/modules/product-form/product-form.js
+++ src/modules/product-form/product-form.js
@@ -3,11 +3,14 @@
  * @constructor
  * @param {Object} el - The site's productform container element.
  */
+
+import { addClass, removeClass } from 'lib/util'
+
 const productform = el => {
   const inputEls = el.querySelectorAll('.js-product-input')
   const variantEl = el.querySelector('.js-product-variants')
   const submitButtonEl = el.querySelector('.js-form-submit')
-  const priceVariants = el.querySelectorAll('.js-variant-price')
+  const priceEls = el.querySelectorAll('.js-variant-price')
   const options = {}
 
   if (inputEls.length && variantEl) {
@@ -22,30 +25,28 @@ const productform = el => {
           if (optionEl.getAttribute('data-title') === selectedTitle) {
             selectedValue = optionEl.value
             optionEl.selected = true
+          } else {
+            optionEl.selected = false
           }
         }
+
+        // Set value for variant dropdown, fallback to blank value if null
+        variantEl.value = selectedValue
+        // Toggle button in case of variant available or not
         if (selectedValue) {
-          variantEl.value = selectedValue
-
-          if (submitButtonEl) {
-            submitButtonEl.removeAttribute('disabled')
-          }
-          for (const priceVariant of priceVariants) {
-            const variantTitle = priceVariant.getAttribute('data-variant-title')
-
-            console.log('variantTitle', variantTitle.split(' / '))
-
-            const titleContains = variantTitle.split(' / ').some(function (v) { return selectedTitle.indexOf(v) >= 0 })
-            const selectedContains = variantTitle === selectedTitle
-            const displayCondition = selectedContains ? variantTitle === selectedTitle : titleContains
-            console.log(titleContains)
-            console.log(selectedContains)
-            console.log(displayCondition)
-
-            if (titleContains) {
-              priceVariant.style.display = 'block'
+          submitButtonEl.removeAttribute('disabled')
+        } else {
+          submitButtonEl.setAttribute('disabled', true)
+        }
+        // Toggle price display
+        if (priceEls.length) {
+          for (const priceEl of priceEls) {
+            const priceMatchOption = priceEl.getAttribute('data-match-option')
+            const isMatched = selectedTitle.indexOf(priceMatchOption) >= 0
+            if (isMatched) {
+              addClass(priceEl, 'active')
             } else {
-              priceVariant.style.display = 'none'
+              removeClass(priceEl, 'active')
             }
           }
         }
