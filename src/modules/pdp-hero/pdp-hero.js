d0d09739f10d3062e0181de7e110245d2b9f0316
Xuan Thai Do <thai.do@solislab.com>
2021-06-14 11:28:44 +0700

    DIC-105 Add video loop for pdp

--- src/modules/pdp-hero/pdp-hero.js
+++ src/modules/pdp-hero/pdp-hero.js
@@ -3,8 +3,38 @@
  * @param {Object} el - The DOM Node containing the data-module="pdp-hero" attribute.
  */
 
+import { contains } from 'lib/util'
+import Video from '../video/video'
+
 const pdpHero = (el) => {
-  console.warn('initializing pdpHero module')
+  let swiper = null
+  const customizeSwiper = swiper => {
+    const videoEls = swiper.slides
+      .map(slide => slide.querySelector('.js-video'))
+      .filter(videoEl => videoEl && !contains(videoEl, 'is-loaded'))
+    if (videoEls.length) {
+      videoEls.map(videoEl => Video(videoEl))
+    }
+  }
+  const carouselObserver = new MutationObserver((mutationList, observer) => {
+    for (const mutation of mutationList) {
+      const isCarousel = mutation.oldValue.indexOf('js-swiper-container') !== -1
+      const isCarouselInitialized = mutation.oldValue.indexOf('swiper-container-initialized') === -1 && contains(mutation.target, 'swiper-container-initialized')
+      if (isCarousel && isCarouselInitialized) {
+        if (contains(mutation.target.parentElement, 'js-carousel')) {
+          swiper = mutation.target.swiper
+          customizeSwiper(swiper)
+          observer.disconnect()
+        }
+      }
+    }
+  })
+  carouselObserver.observe(el, {
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: ['class'],
+    subtree: true
+  })
 }
 
 export default pdpHero
