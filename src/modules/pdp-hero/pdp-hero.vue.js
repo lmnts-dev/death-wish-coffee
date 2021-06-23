d06f9911687d1f35a751bbcb1fbaaec9bfef8302
Xuan Thai Do <thai.do@solislab.com>
2021-06-23 17:49:10 +0700

    Update carousel image on variant change

--- src/modules/pdp-hero/pdp-hero.vue.js
+++ src/modules/pdp-hero/pdp-hero.vue.js
@@ -1,4 +1,5 @@
 import { contains } from 'lib/util'
+import { mapState } from 'vuex'
 import VImage from '../v-image/v-image.vue'
 import PdpAddToCart from '../pdp-add-to-cart/pdp-add-to-cart.vue'
 import Carousel from '../carousel/carousel'
@@ -21,8 +22,25 @@ export default {
     }
   },
   computed: {
+    ...mapState('pdp', ['selectedVariantId']),
+    selectedVariant () {
+      return this.selectedVariantId && this.product.variants.find(v => v.id === this.selectedVariantId)
+    },
     images () {
       return this.product.media.filter(item => item.media_type === 'image')
+    },
+    selectedImageIndex () {
+      return this.selectedVariant && this.selectedVariant.featured_media && this.images.findIndex(image => image.id === this.selectedVariant.featured_media.id)
+    }
+  },
+  watch: {
+    selectedImageIndex (newValue, oldValue) {
+      if (newValue < 0 || newValue === oldValue) {
+        return
+      }
+      if (this.imageCarousel) {
+        this.imageCarousel.slideToLoop(newValue)
+      }
     }
   },
   mounted () {
