ee526dfff43b6fd5bad982f41cf80314e8d174ef
Jason Kirshner <jasonk@ruckusmarketing.com>
2021-06-25 11:20:56 -0400

    Building product-carousel
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

---
+++ src/modules/product-carousel/product-carousel.js
@@ -0,0 +1,10 @@
+/**
+ * Initializes the site's productcarousel module.
+ * @constructor
+ * @param {Object} el - The site's productcarousel container element.
+ */
+const productcarousel = el => {
+  // console.log(el)
+}
+
+export default productcarousel
