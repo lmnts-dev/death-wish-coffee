73eb2a6dc62c05de4f83a00d0f74273c5d9bc89e
James Kim <jamesk@ruckusmarketing.com>
2021-06-29 13:56:26 -0400

    add upscribe account page module

---
+++ src/modules/upscribe-wrapper/upscribe-wrapper.js
@@ -0,0 +1,34 @@
+/**
+ * Initializes the site's upscribewrapper module.
+ * @constructor
+ * @param {Object} el - The site's upscribewrapper container element.
+ */
+
+import Vue from 'vue'
+
+const upscribewrapper = el => {
+  const vue = new Vue({
+    el: '#subscription-wrapper',
+    delimiters: ['${', '}'],
+    data: {
+      showPortal: false
+    },
+    methods: {
+      showUpscribe: function () {
+        this.showPortal = true
+        console.log('show', this.show)
+      }
+    },
+
+    // mounted: {
+
+    // }
+    mounted () {
+      console.log('upscribe', el)
+    }
+  })
+
+  return vue
+}
+
+export default upscribewrapper
