b790b6f58edcca5420a67e2db2fb07eb253c8b92
Xuan Thai Do <thai.do@solislab.com>
2021-09-29 16:27:05 +0700

    DIC-190 Add rewards wrapper

---
+++ src/modules/rewards-wrapper/rewards-wrapper.js
@@ -0,0 +1,19 @@
+/**
+ * Initializes the site's rewardswrapper module.
+ * @constructor
+ * @param {Object} el - The site's rewardswrapper container element.
+ */
+
+const rewardswrapper = el => {
+  window.addEventListener('load', () => {
+    console.log('load')
+    console.log(window.Smile)
+    console.log(window.SmileUI)
+    console.log(window.SmileShopify)
+  })
+  console.log(window.Smile)
+  console.log(window.SmileUI)
+  console.log(window.SmileShopify)
+}
+
+export default rewardswrapper
