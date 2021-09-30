765070f8f7bca51b1c0aa6a4fca291fec1beaf68
Ngoc Nguyen <ngoc.nguyen@solislab.com>
2021-09-30 11:11:15 +0700

    DIC-185 Add rewards intro module

---
+++ src/modules/rewards-intro/rewards-intro.js
@@ -0,0 +1,10 @@
+/**
+ * Initializes the site's rewardsIntro module.
+ * @constructor
+ * @param {Object} el - The site's rewardsIntro container element.
+ */
+
+const rewardsIntro = el => {
+}
+
+export default rewardsIntro
