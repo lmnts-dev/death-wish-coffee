350b7480c55352274bc86246627fbbe00f854cd9
Xuan Thai Do <thai.do@solislab.com>
2021-10-01 13:51:19 +0700

    DIC-186 Fetch informations from smile.io

--- src/modules/rewards-wrapper/rewards-wrapper.js
+++ src/modules/rewards-wrapper/rewards-wrapper.js
@@ -5,8 +5,10 @@
  */
 
 import Vue from 'vue'
+import axios from 'axios'
 import RewardsHero from '../rewards-hero/rewards-hero.vue'
 
+// @TODO: This line is used for Vue debugging. Remove after finished this task.
 Vue.config.devtools = true
 
 const rewardswrapper = el => {
@@ -19,14 +21,45 @@ const rewardswrapper = el => {
     },
     data () {
       return {
-        rewardsCustomer: null
+        rewardsCustomer: null,
+        smileChannel: null
       }
     },
     created () {
-      document.addEventListener('smile-ui-loaded', async () => {
-        const rewardsCustomer = await window.Smile.fetchCustomer({ include: 'vip_tier' })
-        this.rewardsCustomer = rewardsCustomer
+      document.addEventListener('smile-shopify-loaded', async () => {
+        const channelKey = window.Smile.channel_key
+        await this.getChannelData(channelKey)
+        window.SmileShopify.on('customer-identified', async () => {
+          this.reloadCustomer()
+        })
       })
+    },
+    methods: {
+      async getChannelData (channelKey) {
+        if (!channelKey) {
+          return
+        }
+        try {
+          const smileResponse = await axios.get('https://platform.smile.io/v1/smile_ui/init', {
+            params: { channel_key: channelKey },
+            headers: {
+              'smile-channel-key': channelKey,
+              'smile-client': 'smile-ui'
+            }
+          })
+          this.smileChannel = smileResponse.data
+        } catch (error) {
+          console.log('Error loading Smile.io')
+        }
+      },
+      async reloadCustomer () {
+        try {
+          const rewardsCustomer = await window.Smile.fetchCustomer({ include: 'vip_tier' })
+          this.rewardsCustomer = rewardsCustomer
+        } catch (error) {
+          console.log('Error fetching customer data')
+        }
+      }
     }
   })
 }
