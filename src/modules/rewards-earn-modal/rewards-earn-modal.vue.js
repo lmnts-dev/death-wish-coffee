f6b002240ee2872015b00fb671e52ad784d33061
Xuan Thai Do <thai.do@solislab.com>
2021-10-13 10:22:52 +0700

    Add birthday form

--- src/modules/rewards-earn-modal/rewards-earn-modal.vue.js
+++ src/modules/rewards-earn-modal/rewards-earn-modal.vue.js
@@ -8,7 +8,12 @@ export default {
   },
   data () {
     return {
-      isActive: false
+      isActive: false,
+      isBirthdayFormActive: false,
+      birthdayMonthFirst: '',
+      birthdayMonthLast: '',
+      birthdayDayFirst: '',
+      birthdayDayLast: ''
     }
   },
   methods: {
@@ -19,6 +24,48 @@ export default {
     deactivate () {
       this.isActive = false
       removeClass(document.body, 'is-modal-active')
+    },
+    back () {
+      this.isBirthdayFormActive = false
+    },
+    async earnPoints (activity) {
+      switch (activity.activity_rule.type) {
+        case 'shopify_online_order': {
+          window.location.href = '/collections'
+          break
+        }
+        case 'customer_birthday': {
+          this.isBirthdayFormActive = true
+          break
+        }
+        case 'signup': {
+          window.location.href = '/account/register'
+          break
+        }
+        default: {
+          await window.Smile.createActivity({ token: activity.activity_rule.type })
+        }
+      }
+    },
+    focusInput (e) {
+      const inputEl = e.currentTarget
+      const value = e.currentTarget.value
+      const length = value ? value.length : 0
+      inputEl.setSelectionRange(0, length)
+    },
+    nextInput (e) {
+      const inputEl = e.currentTarget
+      const inputMaxLength = inputEl.getAttribute('maxlength') ? parseInt(inputEl.getAttribute('maxlength')) : 0
+      const value = inputEl.value
+      const index = inputEl.getAttribute('data-index') ? parseInt(inputEl.getAttribute('data-index')) : 0
+      const nextInputRef = index && `input${index + 1}`
+      const nextInputEl = nextInputRef ? this.$refs[nextInputRef] : null
+      if (inputMaxLength && value.length === inputMaxLength && nextInputEl) {
+        nextInputEl.focus()
+      }
+    },
+    saveDate () {
+
     }
   }
 }
