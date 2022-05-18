be6d063f5cfb098077022848f3a1ff81bc4a7a85
Alisha Garric <alisha@laxaltandmciver.co>
2022-05-18 13:57:07 -0500

    :rocket: deploying: govx snippet addition and style overrides

---
+++ src/modules/govx-id/govx-id.js
@@ -0,0 +1,10 @@
+/**
+ * Initializes the javascript for the govx-id
+ * @param {Object} el - The DOM Node containing the data-module="govx-id" attribute.
+ */
+
+const govxId = (el) => {
+  console.warn('initializing govx-id')
+}
+
+export default govxId
