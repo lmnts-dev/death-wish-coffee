eebcaa25278b572191141fcfbf6f69bc08c794d7
Don Morrison <don@mutations.ltd>
2022-06-22 17:18:00 -0700

    [og] Create og-account-link module
    Adds a module to render the correct subscription account link

---
+++ src/modules/og-account-link/og-account-link.js
@@ -0,0 +1,10 @@
+/**
+ * Initializes the javascript for the og-offer module
+ * @param {Object} el - The DOM Node containing the data-module="og-account-link" attribute.
+ */
+
+const ogAccountLink = (el) => {
+  console.warn('initializing og-account-link module')
+}
+
+export default ogAccountLink
