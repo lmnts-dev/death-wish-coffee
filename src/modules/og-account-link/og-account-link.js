e2ec99a86e2f09ddb1fb64aeb47265c52ddf0eeb
Don Morrison <don@mutations.ltd>
2022-06-22 18:16:56 -0700

    [og] Update og-account-link module
    Updates the module to create og_auth cookie so SMI can load.

--- src/modules/og-account-link/og-account-link.js
+++ src/modules/og-account-link/og-account-link.js
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie'
+
 /**
  * Initializes the javascript for the og-offer module
  * @param {Object} el - The DOM Node containing the data-module="og-account-link" attribute.
@@ -5,6 +7,37 @@
 
 const ogAccountLink = (el) => {
   console.warn('initializing og-account-link module')
+  if (!el) return
+
+  const customerId = el.getAttribute('data-og-customer-id')
+  const timestamp = el.getAttribute('data-og-timestamp')
+  const signature = el.getAttribute('data-og-signature')
+
+  // Create a new `og_auth` cookie each time this module is initialized
+  writeOgAuthCookie(customerId, timestamp, signature)
 }
 
+/**
+ * Create the cookie needed by the Ordergroove SMI authentication
+ *
+ * https://og-restrpc.readme.io/docs/integration-reference-material#authentication-page
+ *
+ * @param {*} customerId
+ * @param {*} timestamp
+ * @param {*} signature
+ */
+const writeOgAuthCookie = (customerId, timestamp, signature) => {
+  const value = `${customerId}|${timestamp}|${signature}`
+  // Expires in 2 hours (120 minutes)
+  const expires = new Date(new Date().getTime() + 120 * 60 * 1000)
+
+  return Cookies.set(
+    'og_auth',
+    value, {
+      expires,
+      path: '/',
+      secure: true
+    }
+  )
+}
 export default ogAccountLink
