a34ab672694fb07e065823f4e2c7a0dcef91acc3
Don Morrison <don@mutations.ltd>
2022-06-27 19:52:17 -0700

    [og] product-form: Remove unused getParameterByName

--- src/modules/product-form/product-form.vue.js
+++ src/modules/product-form/product-form.vue.js
@@ -448,14 +448,6 @@ export default {
         {}
       )
     },
-    getParameterByName (name, url = window.location.href) {
-      name = name.replace(/[\]]/g, '\\$&')
-      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
-      var results = regex.exec(url)
-      if (!results) return null
-      if (!results[2]) return ''
-      return decodeURIComponent(results[2].replace(/\+/g, ' '))
-    },
     getIndex (index) {
       console.log(index)
       this.index = index
