55ae4f2261fd92bda4bf11dbaef1a0ac08e9fbdf
alishagarric <alisha@laxaltandmciver.co>
2022-06-29 12:12:00 -0500

    :rocket: deploying: add smile-initializer and shogun-head snippet files as modules which were added directly to production during the developer gap

---
+++ src/modules/shogun-head/shogun-head.js
@@ -0,0 +1,11 @@
+/**
+ * Initializes the site's shogun-head module.
+ * @constructor
+ * @param {Object} el - The site's shogun-head container element.
+ */
+
+const shogunHead = el => {
+  console.warn('shogun head module initialized')
+}
+
+export default shogunHead
