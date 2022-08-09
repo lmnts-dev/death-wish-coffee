eea683539aef3a8785e8375f32e934a446b1a1b8
alishagarric <alisha@laxaltandmciver.co>
2022-08-09 14:55:54 -0700

    :rocket: deploying: add shogun files that were added directly to the live theme

---
+++ src/modules/shogun-products/shogun-products.js
@@ -0,0 +1,11 @@
+/**
+ * Initializes the site's shogun-products module.
+ * @constructor
+ * @param {Object} el - The site's shogun-products container element.
+ */
+
+const shogunProducts = el => {
+  console.warn("shogun products module initialized");
+}
+
+export default shogunProducts
