02297018a6241626fb7516cfb64600de7bcb18a9
Don Morrison <don@mutations.ltd>
2022-08-13 21:15:48 -0700

    [og] og-offer: Update module to update subscription button prices
    Updates the `og-offer` to update the prices on the subscription buttons
    so that variants, other than the default, display the correct prices.

--- src/modules/og-offer/og-offer.vue.js
+++ src/modules/og-offer/og-offer.vue.js
@@ -34,6 +34,9 @@ export default {
       type: String,
       required: true
     },
+    selectedVariant: {
+      type: Object
+    }
   },
   data() {
     return {
@@ -96,18 +99,14 @@ export default {
     },
 
     /**
-     * Reference to the DOM node where Ordergroove stores selling plan map
+     * Reference to the DOM node where Ordergroove stores selling plan map.
      *
      * @returns Vue Ref
      */
     sellingPlanMap() {
-      const frequency = this.frequency
-      const variantId = this.variantId
+      const map = this.getSellingPlanMap(this.variantId, this.frequency)
 
-      const refKey = `${variantId}-${frequency}`
-      const map = this.$refs[refKey]
-
-      debug('sellingPlanMap', refKey, map)
+      debug('sellingPlanMap', map)
 
       return map
     },
@@ -192,6 +191,39 @@ export default {
     },
 
     /**
+     * Update prices on the subscription buttons.
+     *
+     * There is an issue with Ordergroove where prices for any variant
+     * other than the default are not updated correctly. This function
+     * overwrites the `shadow-root` for the `og-price` node with prices
+     * from the selling plan map for the specific frequency.
+     */
+    $_updateSubscriptionButtonsPrices() {
+      const buttonsContainer = document.querySelector('div.og-optin-buttons')
+      if (!buttonsContainer) return
+
+      const buttons = buttonsContainer.querySelectorAll('og-optin-button')
+      if (!buttons || buttons.length === 0) return
+
+      // Update the price for each button
+      buttons.forEach(button => {
+        const frequency = button.getAttribute('default-frequency')
+        const plan = this.getSellingPlanMap(this.variantId, frequency)
+        const price = plan.getAttribute('data-price')
+
+        const ogPrice = button.querySelector('og-price')
+
+        if (ogPrice && ogPrice.shadowRoot) {
+          // eslint-disable-next-line no-template-curly-in-string
+          const formattedPrice = formatMoney(price, '${{amount}}')
+
+          ogPrice.shadowRoot.innerHTML = buildButtonPriceDocumentFragment(formattedPrice)
+          debug('$_updateSubscriptionButtonsPrices', frequency, formattedPrice)
+        }
+      })
+    },
+
+    /**
      * Update window.order_line_items for Ordergroove.
      *
      * Code adapted from the `<script>` tag in the original liquid template.
@@ -199,6 +231,20 @@ export default {
     $_updateWindowLineItems() {
       window.order_line_items = this.productIds
       debug('$_updateWindowLineItems', this.productIds)
+    },
+
+    /**
+     * Get the ref that stores the plan map for the variant and frequency.
+     *
+     * @returns Vue Ref
+     */
+    getSellingPlanMap(variantId, frequency) {
+      const refKey = `${variantId}-${frequency}`
+      const map = this.$refs[refKey]
+
+      debug('getSellingPlanMap', refKey, map)
+
+      return map
     },
   },
   watch: {
@@ -211,11 +257,21 @@ export default {
     },
 
     /**
+     * Watch `selectedVariant` prop for updates.
+     */
+    selectedVariant() {
+      debug('[watch] selectedVariant', this.selectedVariant)
+    },
+
+    /**
      * Watch `variantId` for updates.
      */
     variantId() {
       debug('[watch] variantId', this.variantId)
       this.$_updateFrequencyData()
+
+      // Ensure DOM is updated before updating subscription buttons
+      Vue.nextTick(this.$_updateSubscriptionButtonsPrices)
     },
   },
 }
@@ -227,6 +283,99 @@ function debug() {
   if (DEBUG) {
     console.debug('[og-offer] ', ...arguments)
   }
+}
+
+/**
+ * Build a document fragment for a subscription button price.
+ *
+ * @param {String} price
+ * @returns String
+ */
+function buildButtonPriceDocumentFragment(price) {
+  return `
+    <!---->
+    <slot name="prepend"></slot>
+    ${price}
+    <slot name="append"></slot>
+    <!---->
+  `
+}
+
+/**
+ * Shopify format money.
+ *
+ * @param {Number | String} cents
+ * @param {String} format
+ * @returns String
+ */
+function formatMoney(cents, format) {
+  if (typeof cents === 'string') {
+    cents = cents.replace('.', '')
+  }
+
+  var value = ''
+  // Regex to match a liquid-type token in a string, i.e. {{amount}}
+  var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/
+
+  // Find matched "group" in the format string, i.e. {{amount}} -> amount
+  const match = format.match(placeholderRegex)[1]
+
+  switch (match) {
+    case 'amount':
+      value = formatWithDelimiters(cents, 2)
+      break
+
+    case 'amount_no_decimals':
+      value = formatWithDelimiters(cents, 0)
+      break
+
+    case 'amount_with_comma_separator':
+      value = formatWithDelimiters(cents, 2, '.', ',')
+      break
+
+    case 'amount_no_decimals_with_comma_separator':
+      value = formatWithDelimiters(cents, 0, '.', ',')
+      break
+
+    case 'amount_no_decimals_with_space_separator':
+      value = formatWithDelimiters(cents, 0, ' ')
+      break
+
+    case 'amount_with_apostrophe_separator':
+      value = formatWithDelimiters(cents, 2, "'")
+      break
+  }
+  debug('formatMoney', { cents, format, value, match })
+  return format.replace(placeholderRegex, value)
+}
+
+/**
+ * Format a number with delimiters.
+ *
+ * @param {*} number
+ * @param {*} precision
+ * @param {*} thousands
+ * @param {*} decimal
+ * @returns
+ */
+function formatWithDelimiters(number, precision, thousands, decimal) {
+  thousands = thousands || ','
+  decimal = decimal || '.'
+
+  if (isNaN(number) || number === null) {
+    return 0
+  }
+
+  number = (number / 100.0).toFixed(precision)
+
+  var parts = number.split('.')
+  var dollarsAmount = parts[0].replace(
+    /(\d)(?=(\d\d\d)+(?!\d))/g,
+    '$1' + thousands
+  )
+  var centsAmount = parts[1] ? decimal + parts[1] : ''
+
+  return dollarsAmount + centsAmount
 }
 
 /* eslint-enable comma-dangle,space-before-function-paren */
